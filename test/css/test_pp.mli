7d12d721ffde941e3d196659d4cafb9ce74aaa0e
Thomas Gazagnaire <thomas@gazagnaire.org>
2025-09-04 20:37:18 +0200

    Add CSS pretty-printer module and documentation
    - Add lib/css/pp.ml and pp.mli for CSS AST pretty-printing
    - Add test files for CSS pretty-printer
    - Add test/css/parser/dune for parser test configuration
    - Add todo documentation for aria attributes and CSS parser

---
+++ test/css/test_pp.mli
@@ -0,0 +1,4 @@
+(** Test suite for CSS pretty-printing module *)
+
+val suite : (string * unit Alcotest.test_case list) list
+(** [suite] provides test suite for CSS pretty-printing functionality. *)
